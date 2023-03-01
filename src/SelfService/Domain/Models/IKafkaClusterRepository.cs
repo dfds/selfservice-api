a3ea768b19ce21ba38867710a528e3081bac424e
Jens Andresen <jandr@DFDS.COM>
2023-03-01 15:21:20 +0100

    add check for kafka cluster exists when requesting a topic

---
+++ src/SelfService/Domain/Models/IKafkaClusterRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace SelfService.Domain.Models;
+
+public interface IKafkaClusterRepository
+{
+    Task<bool> Exists(KafkaClusterId id);
+}
---
+++ src/SelfService/Infrastructure/Persistence/KafkaClusterRepository.cs
@@ -0,0 +1,19 @@
+﻿using SelfService.Domain.Models;
+
+namespace SelfService.Infrastructure.Persistence;
+
+public class KafkaClusterRepository : IKafkaClusterRepository
+{
+    private readonly SelfServiceDbContext _dbContext;
+
+    public KafkaClusterRepository(SelfServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<bool> Exists(KafkaClusterId id)
+    {
+        var found = await _dbContext.KafkaClusters.FindAsync(id);
+        return found != null;
+    }
+}
