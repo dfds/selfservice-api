7243279eebe1018f8ab75cd18cd93412fcaeee5e
Jens Andresen <jandr@DFDS.COM>
2023-03-01 08:53:56 +0100

    add missing db migration

---
+++ db/migrations/20230228151735_add-missing-migrations-for-kafkatopic-table.sql
@@ -0,0 +1,12 @@
+-- 2023-02-28 15:17:35 : add-missing-migrations-for-kafkatopic-table
+
+alter table "KafkaTopic"
+	drop constraint "KafkaTopic_CapabilityId_KafkaClusterId_Name_key";
+
+-- update prod
+update "KafkaTopic" set "KafkaClusterId" = 'lkc-4npj6'
+where "KafkaClusterId" = '92e49432-d3d1-4e6c-b5ab-f7b7cb7c9a9b';
+
+-- update dev
+update "KafkaTopic" set "KafkaClusterId" = 'lkc-3wqzw'
+where "KafkaClusterId" = '9cc6e73a-f62a-4f12-8fb5-3b489e39fb0a';
