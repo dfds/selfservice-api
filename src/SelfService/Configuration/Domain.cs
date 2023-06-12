42794520fcd398959dc4b1058aa0fa3e350eea55
paulseghers <0099seghers@gmail.com>
2023-06-12 16:20:17 +0200

    WIP - removal of inactive users

--- src/SelfService/Configuration/Domain.cs
+++ src/SelfService/Configuration/Domain.cs
@@ -66,6 +66,7 @@ public static class Domain
         
         // background jobs
         builder.Services.AddHostedService<CancelExpiredMembershipApplications>();
+        builder.Services.AddHostedService<RemoveInactiveMemberships>();
         builder.Services.AddHostedService<PortalVisitAnalyzer>();
 
         // misc
