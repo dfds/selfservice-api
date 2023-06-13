1e2039a60bbee317e3c04b7b6339c7e81b1c60d2
Kristian Pilegaard Jensen <mac@Kristians-MacBook-Pro.local>
2023-06-13 09:48:40 +0200

    Commented out line preventing api from starting

--- src/SelfService/Configuration/Domain.cs
+++ src/SelfService/Configuration/Domain.cs
@@ -66,7 +66,7 @@ public static class Domain
         
         // background jobs
         builder.Services.AddHostedService<CancelExpiredMembershipApplications>();
-        builder.Services.AddHostedService<RemoveInactiveMemberships>();
+        //builder.Services.AddHostedService<RemoveInactiveMemberships>();
         builder.Services.AddHostedService<PortalVisitAnalyzer>();
 
         // misc
