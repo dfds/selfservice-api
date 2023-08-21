4b9e8797ca3cd3334b576a1659bebcc1479ecfb9
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-08-21 11:21:12 +0200

    formatting

--- src/SelfService.Tests/Infrastructure/Messaging/TestDomainEventsDafdaDeserialization.cs
+++ src/SelfService.Tests/Infrastructure/Messaging/TestDomainEventsDafdaDeserialization.cs
@@ -30,12 +30,13 @@ public class TestDomainEventsDafdaDeserialization
     private const string TestSchema = "schema";
     private const string TestMemberShipId = "membershipId";
 
-
-    private async Task dafda_serialize_deserialize<T>(T domainEvent) where T : IDomainEvent
+    private async Task dafda_serialize_deserialize<T>(T domainEvent)
+        where T : IDomainEvent
     {
         DefaultPayloadSerializer serializer = new DefaultPayloadSerializer();
-        var serialized = await serializer.Serialize(new PayloadDescriptor("", "", "", "", domainEvent,
-            new KeyValuePair<string, string>[] { }));
+        var serialized = await serializer.Serialize(
+            new PayloadDescriptor("", "", "", "", domainEvent, new KeyValuePair<string, string>[] { })
+        );
 
         JsonSerializer.Deserialize(serialized, typeof(T), _jsonSerializerOptions);
     }
@@ -57,123 +58,136 @@ public class TestDomainEventsDafdaDeserialization
     public async Task dafda_serialize_deserialize_kafka_cluster_access_requested()
     {
         await dafda_serialize_deserialize(new KafkaClusterAccessRequested());
-        await dafda_serialize_deserialize(new KafkaClusterAccessRequested()
-            { CapabilityId = TestCapabilityId, KafkaClusterId = TestKafkaClusterId });
+        await dafda_serialize_deserialize(
+            new KafkaClusterAccessRequested() { CapabilityId = TestCapabilityId, KafkaClusterId = TestKafkaClusterId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_kafka_topic_deleted()
     {
         await dafda_serialize_deserialize(new KafkaTopicHasBeenDeleted());
-        await dafda_serialize_deserialize(new  KafkaTopicHasBeenDeleted() { KafkaTopicId = TestTopicId });
+        await dafda_serialize_deserialize(new KafkaTopicHasBeenDeleted() { KafkaTopicId = TestTopicId });
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_membership_application_cancelled()
     {
         await dafda_serialize_deserialize(new MembershipApplicationHasBeenCancelled());
-        await dafda_serialize_deserialize(new MembershipApplicationHasBeenCancelled()
-            { MembershipApplicationId = TestApplicationId });
+        await dafda_serialize_deserialize(
+            new MembershipApplicationHasBeenCancelled() { MembershipApplicationId = TestApplicationId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_membership_application_finalized()
     {
         await dafda_serialize_deserialize(new MembershipApplicationHasBeenFinalized());
-        await dafda_serialize_deserialize(new MembershipApplicationHasBeenFinalized() { MembershipApplicationId = TestApplicationId });
+        await dafda_serialize_deserialize(
+            new MembershipApplicationHasBeenFinalized() { MembershipApplicationId = TestApplicationId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_membership_application_approval_received()
     {
         await dafda_serialize_deserialize(new MembershipApplicationHasReceivedAnApproval());
-        await dafda_serialize_deserialize(new MembershipApplicationHasReceivedAnApproval() { MembershipApplicationId = TestApplicationId });
+        await dafda_serialize_deserialize(
+            new MembershipApplicationHasReceivedAnApproval() { MembershipApplicationId = TestApplicationId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_new_kafka_topic_requested()
     {
         await dafda_serialize_deserialize(new NewKafkaTopicHasBeenRequested());
-        await dafda_serialize_deserialize(new NewKafkaTopicHasBeenRequested
-        {
-            CapabilityId = TestCapabilityId,
-            KafkaClusterId = TestKafkaClusterId,
-            KafkaTopicId = TestTopicId,
-            KafkaTopicName = TestTopicName
-        });
+        await dafda_serialize_deserialize(
+            new NewKafkaTopicHasBeenRequested
+            {
+                CapabilityId = TestCapabilityId,
+                KafkaClusterId = TestKafkaClusterId,
+                KafkaTopicId = TestTopicId,
+                KafkaTopicName = TestTopicName
+            }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_new_membership_application_submitted()
     {
         await dafda_serialize_deserialize(new NewMembershipApplicationHasBeenSubmitted());
-        await dafda_serialize_deserialize(new NewMembershipApplicationHasBeenSubmitted
-        {
-            MembershipApplicationId = TestApplicationId
-        });
+        await dafda_serialize_deserialize(
+            new NewMembershipApplicationHasBeenSubmitted { MembershipApplicationId = TestApplicationId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_new_message_contract_provisioned()
     {
         await dafda_serialize_deserialize(new NewMessageContractHasBeenProvisioned());
-        await dafda_serialize_deserialize(new NewMessageContractHasBeenProvisioned
-        {
-            KafkaTopicId = TestTopicId,
-            MessageContractId = TestMessageContractId,
-            MessageType = TestMessageType
-        });
+        await dafda_serialize_deserialize(
+            new NewMessageContractHasBeenProvisioned
+            {
+                KafkaTopicId = TestTopicId,
+                MessageContractId = TestMessageContractId,
+                MessageType = TestMessageType
+            }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_new_message_contract_requested()
     {
         await dafda_serialize_deserialize(new NewMessageContractHasBeenRequested());
-        await dafda_serialize_deserialize(new NewMessageContractHasBeenRequested
-        {
-            CapabilityId = TestCapabilityId,
-            Description = TestDescription,
-            KafkaClusterId = TestKafkaClusterId,
-            KafkaTopicId = TestTopicId,
-            KafkaTopicName = TestTopicName,
-            MessageContractId = TestMessageContractId,
-            MessageType = TestMessageType,
-            Schema = TestSchema,
-        });
+        await dafda_serialize_deserialize(
+            new NewMessageContractHasBeenRequested
+            {
+                CapabilityId = TestCapabilityId,
+                Description = TestDescription,
+                KafkaClusterId = TestKafkaClusterId,
+                KafkaTopicId = TestTopicId,
+                KafkaTopicName = TestTopicName,
+                MessageContractId = TestMessageContractId,
+                MessageType = TestMessageType,
+                Schema = TestSchema,
+            }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_new_portal_visit_registered()
     {
         await dafda_serialize_deserialize(new NewPortalVisitRegistered());
-        await dafda_serialize_deserialize(new NewPortalVisitRegistered
-        {
-            PortalVisitId = TestPortalVisitId,
-            VisitedAt = "now",
-            VisitedBy = TestUser
-        });
+        await dafda_serialize_deserialize(
+            new NewPortalVisitRegistered
+            {
+                PortalVisitId = TestPortalVisitId,
+                VisitedAt = "now",
+                VisitedBy = TestUser
+            }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_user_joined_capability()
     {
         await dafda_serialize_deserialize(new UserHasJoinedCapability());
-        await dafda_serialize_deserialize(new UserHasJoinedCapability
-        {
-            CapabilityId = TestCapabilityId,
-            MembershipId = TestMemberShipId
-        });
+        await dafda_serialize_deserialize(
+            new UserHasJoinedCapability { CapabilityId = TestCapabilityId, MembershipId = TestMemberShipId }
+        );
     }
 
     [Fact]
     public async Task dafda_serialize_deserialize_user_left_capability()
     {
         await dafda_serialize_deserialize(new UserHasLeftCapability());
-        await dafda_serialize_deserialize(new UserHasLeftCapability
-        {
-            CapabilityId = TestCapabilityId,
-            MembershipId = TestMemberShipId,
-            UserId = TestUser
-        });
+        await dafda_serialize_deserialize(
+            new UserHasLeftCapability
+            {
+                CapabilityId = TestCapabilityId,
+                MembershipId = TestMemberShipId,
+                UserId = TestUser
+            }
+        );
     }
 }
