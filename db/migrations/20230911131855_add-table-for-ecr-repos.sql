3f71dd36d1d5fbb89be048ca9aa7b13aadb0b08c
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-09-11 13:27:39 +0200

    added missing migration

---
+++ db/migrations/20230911131855_add-table-for-ecr-repos.sql
@@ -0,0 +1,12 @@
+-- 2023-09-11 13:18:55 : add-table-for-ecr-repos
+
+create table "ECRRepository"
+(
+    "Id"             UUID          NOT NULL,
+    "Name"           VARCHAR(255)  NOT NULL,
+    "Description"    VARCHAR(1024) NOT NULL,
+    "RepositoryName" VARCHAR(255)  NOT NULL,
+    "CreatedBy"      VARCHAR(255)  NOT NULL,
+
+    CONSTRAINT "ECRRepository_PK" PRIMARY KEY ("Id")
+);
--- src/SelfService/Domain/Models/ECRRepository.cs
+++ src/SelfService/Domain/Models/ECRRepository.cs
@@ -2,7 +2,6 @@ namespace SelfService.Domain.Models;
 
 public class ECRRepository : Entity<ECRRepositoryId>
 {
-    public ECRRepositoryId EcrRepositoryId { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public string RepositoryName { get; private set; }
@@ -11,7 +10,6 @@ public class ECRRepository : Entity<ECRRepositoryId>
     public ECRRepository(ECRRepositoryId id, string name, string description, string repositoryName, string createdBy)
         : base(id)
     {
-        EcrRepositoryId = id;
         Name = name;
         Description = description;
         RepositoryName = repositoryName;
