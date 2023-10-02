38a575a8e6270d7792d831dca430ebcd813465a6
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-10-02 13:21:35 +0200

    created setup function, fixed tests

--- src/SelfService.Tests/Application/TestCapabilityApplicationService.cs
+++ src/SelfService.Tests/Application/TestCapabilityApplicationService.cs
@@ -1,22 +1,26 @@
 using Moq;
+using SelfService.Application;
 using SelfService.Domain.Exceptions;
 using SelfService.Domain.Models;
+using SelfService.Infrastructure.Persistence;
 
 namespace SelfService.Tests.Application;
 
 public class TestCapabilityApplicationService
 {
-    private const string TestJsonMetadata = """
-                                                {
-                                                    "foo": "bar"
-                                                }
-                                            """;
+    private const string JsonMetadata = """
+                                            {
+                                                "foo": "bar"
+                                            }
+                                        """;
 
     private const string InvalidJsonMetadata = """
                                                    {
                                                        "not": "valid"
                                                    }
                                                """;
+
+    private const string SchemaEmpty = """{}""";
 
     private const string SchemaWithRequiredField = """
                                                    {
@@ -34,23 +38,47 @@ public class TestCapabilityApplicationService
 
     private readonly Mock<ISelfServiceJsonSchemaRepository> _mock = new();
 
-    [Fact]
-    public async Task can_set_json_metadata()
+    private SelfServiceJsonSchema CreateSchema(int version, string schemaString)
     {
-        await using var databaseFactory = new InMemoryDatabaseFactory();
-        var dbContext = await databaseFactory.CreateDbContext();
+        return new SelfServiceJsonSchema(version, SelfServiceJsonSchemaObjectId.Capability, schemaString);
+    }
+
+    /// <summary>
+    /// Creates a capability, repository and service for testing json metadata
+    /// </summary>
+    /// <param name="schema">The schema you wish to use for the test</param>
+    /// <param name="dbContext">Must be passed in to avoid being Disposed</param>
+    private async Task<(
+        Capability,
+        CapabilityRepository repo,
+        CapabilityApplicationService capabilityService
+    )> SetupCapabilityMetadataTesting(SelfServiceJsonSchema schema, SelfServiceDbContext dbContext)
+    {
         var repo = A.CapabilityRepository.WithDbContext(dbContext).Build();
 
         var newCapability = A.Capability.Build();
         await repo.Add(newCapability);
         await dbContext.SaveChangesAsync();
 
-        var capabilityService = A.CapabilityApplicationService.WithCapabilityRepository(repo).Build();
+        _mock.Setup(x => x.GetLatestSchema(SelfServiceJsonSchemaObjectId.Capability)).ReturnsAsync(() => schema);
+        var sut = A.SelfServiceJsonSchemaService.WithJsonSchemaRepository(_mock.Object).Build();
+        var capabilityService = A.CapabilityApplicationService
+            .WithCapabilityRepository(repo)
+            .WithSelfServiceJsonSchemaService(sut)
+            .Build();
+        return (newCapability, repo, capabilityService);
+    }
 
-        await capabilityService.SetJsonMetadata(newCapability.Id, TestJsonMetadata);
+    [Fact]
+    public async Task can_set_json_metadata()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext();
+        var (capability, repo, service) = await SetupCapabilityMetadataTesting(CreateSchema(0, SchemaEmpty), dbContext);
 
-        var capabilityWithJsonMetadata = await repo.Get(newCapability.Id);
-        Assert.Equal(TestJsonMetadata, capabilityWithJsonMetadata.JsonMetadata);
+        await service.SetJsonMetadata(capability.Id, JsonMetadata);
+        var capabilityWithJsonMetadata = await repo.Get(capability.Id);
+        Assert.Equal(JsonMetadata, capabilityWithJsonMetadata.JsonMetadata);
         Assert.Equal(0, capabilityWithJsonMetadata.JsonMetadataSchemaVersion);
     }
 
@@ -59,28 +87,15 @@ public class TestCapabilityApplicationService
     {
         await using var databaseFactory = new InMemoryDatabaseFactory();
         var dbContext = await databaseFactory.CreateDbContext();
-        var capabilityRepository = A.CapabilityRepository.WithDbContext(dbContext).Build();
+        var (capability, repo, service) = await SetupCapabilityMetadataTesting(
+            CreateSchema(1, SchemaWithRequiredField),
+            dbContext
+        );
 
-        var newCapability = A.Capability.Build();
-        await capabilityRepository.Add(newCapability);
-        await dbContext.SaveChangesAsync();
+        await service.SetJsonMetadata(capability.Id, JsonMetadata);
 
-        _mock
-            .Setup(x => x.GetLatestSchema(SelfServiceJsonSchemaObjectId.Capability))
-            .ReturnsAsync(
-                () => new SelfServiceJsonSchema(1, SelfServiceJsonSchemaObjectId.Capability, SchemaWithRequiredField)
-            );
-        var sut = A.SelfServiceJsonSchemaService.WithJsonSchemaRepository(_mock.Object).Build();
-
-        var capabilityService = A.CapabilityApplicationService
-            .WithCapabilityRepository(capabilityRepository)
-            .WithSelfServiceJsonSchemaService(sut)
-            .Build();
-
-        await capabilityService.SetJsonMetadata(newCapability.Id, TestJsonMetadata);
-
-        var capabilityWithJsonMetadata = await capabilityRepository.Get(newCapability.Id);
-        Assert.Equal(TestJsonMetadata, capabilityWithJsonMetadata.JsonMetadata);
+        var capabilityWithJsonMetadata = await repo.Get(capability.Id);
+        Assert.Equal(JsonMetadata, capabilityWithJsonMetadata.JsonMetadata);
         Assert.Equal(1, capabilityWithJsonMetadata.JsonMetadataSchemaVersion);
     }
 
@@ -89,26 +104,13 @@ public class TestCapabilityApplicationService
     {
         await using var databaseFactory = new InMemoryDatabaseFactory();
         var dbContext = await databaseFactory.CreateDbContext();
-        var capabilityRepository = A.CapabilityRepository.WithDbContext(dbContext).Build();
-
-        var newCapability = A.Capability.Build();
-        await capabilityRepository.Add(newCapability);
-        await dbContext.SaveChangesAsync();
-
-        _mock
-            .Setup(x => x.GetLatestSchema(SelfServiceJsonSchemaObjectId.Capability))
-            .ReturnsAsync(
-                () => new SelfServiceJsonSchema(1, SelfServiceJsonSchemaObjectId.Capability, SchemaWithRequiredField)
-            );
-        var sut = A.SelfServiceJsonSchemaService.WithJsonSchemaRepository(_mock.Object).Build();
-
-        var capabilityService = A.CapabilityApplicationService
-            .WithCapabilityRepository(capabilityRepository)
-            .WithSelfServiceJsonSchemaService(sut)
-            .Build();
+        var (capability, _, service) = await SetupCapabilityMetadataTesting(
+            CreateSchema(1, SchemaWithRequiredField),
+            dbContext
+        );
 
         await Assert.ThrowsAsync<InvalidJsonMetadataException>(
-            () => capabilityService.SetJsonMetadata(newCapability.Id, InvalidJsonMetadata)
+            () => service.SetJsonMetadata(capability.Id, InvalidJsonMetadata)
         );
     }
 }
