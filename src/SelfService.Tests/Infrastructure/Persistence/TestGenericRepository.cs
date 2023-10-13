748408fc648a90f76e780b3f0d3bf95ac24af8dd
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-10-13 10:56:48 +0200

    added more tests

--- src/SelfService.Tests/Infrastructure/Persistence/TestGenericRepository.cs
+++ src/SelfService.Tests/Infrastructure/Persistence/TestGenericRepository.cs
@@ -56,26 +56,111 @@ public class TestGenericRepository
         }
     }
 
-    [Fact]
-    [Trait("Category", "InMemoryDatabase")]
-    public async Task add_inserts_correctly()
-    {
-        await using var databaseFactory = new InMemoryDatabaseFactory();
-        var dbContext = await databaseFactory.CreateDbContext<TestDbContext>(options => new TestDbContext(options));
+    private static readonly TestGenericRepositoryModelId TestId = TestGenericRepositoryModelId.New();
 
+    private async Task<
+        GenericRepository<TestGenericRepositoryModel, TestGenericRepositoryModelId>
+    > CreateGenericRepoAndAddOne(TestDbContext dbContext)
+    {
         var repo = new GenericRepository<TestGenericRepositoryModel, TestGenericRepositoryModelId>(
             dbContext.TestGenericRepositoryModels
         );
 
-        var id = TestGenericRepositoryModelId.New();
-        await repo.Add(new TestGenericRepositoryModel(id, "foo", 1));
+        await repo.Add(new TestGenericRepositoryModel(TestId, "foo", 1));
+        await dbContext.SaveChangesAsync();
+        return repo;
+    }
+
+    [Fact]
+    [Trait("Category", "InMemoryDatabase")]
+    public async Task can_add_to_repo()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext<TestDbContext>(options => new TestDbContext(options));
+
+        var repo = await CreateGenericRepoAndAddOne(dbContext);
+
+        var allObjects = await repo.GetAll();
+
+        Assert.Single(allObjects);
+        Assert.Equal(TestId, allObjects[0].Id);
+        Assert.Equal("foo", allObjects[0].Foo);
+        Assert.Equal(1, allObjects[0].Bar);
+    }
+
+    [Fact]
+    [Trait("Category", "InMemoryDatabase")]
+    public async Task can_delete_from_repo()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext<TestDbContext>(options => new TestDbContext(options));
+
+        var repo = await CreateGenericRepoAndAddOne(dbContext);
+
+        // sanity check
+        var allObjects = await repo.GetAll();
+        Assert.Single(allObjects);
+
+        await repo.Remove(TestId);
+        await dbContext.SaveChangesAsync();
+        var allObjectsAfterDeletion = await repo.GetAll();
+        Assert.Empty(allObjectsAfterDeletion);
+    }
+
+    [Fact]
+    [Trait("Category", "InMemoryDatabase")]
+    public async Task can_find_by_id()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext<TestDbContext>(options => new TestDbContext(options));
+
+        var repo = await CreateGenericRepoAndAddOne(dbContext);
+
+        var otherId = TestGenericRepositoryModelId.New();
+        await repo.Add(new TestGenericRepositoryModel(otherId, "woo", 10));
+        await dbContext.SaveChangesAsync();
+        var toBeFound = await repo.FindBy(TestId);
+        Assert.NotNull(toBeFound);
+        var toNotBeFound = await repo.FindBy(TestGenericRepositoryModelId.New());
+        Assert.Null(toNotBeFound);
+    }
+
+    [Fact]
+    [Trait("Category", "InMemoryDatabase")]
+    public async Task can_find_with_exists()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext<TestDbContext>(options => new TestDbContext(options));
+
+        var repo = await CreateGenericRepoAndAddOne(dbContext);
+        var doesExist = await repo.Exists(TestId);
+        Assert.True(doesExist);
+        var doesNotExist = await repo.Exists(TestGenericRepositoryModelId.New());
+        Assert.False(doesNotExist);
+    }
+
+    [Fact]
+    [Trait("Category", "InMemoryDatabase")]
+    public async Task using_deal_db_set_works()
+    {
+        await using var databaseFactory = new InMemoryDatabaseFactory();
+        var dbContext = await databaseFactory.CreateDbContext<SelfServiceDbContext>(
+            options => new TestDbContext(options)
+        );
+
+        var repo = new GenericRepository<KafkaTopic, KafkaTopicId>(dbContext.KafkaTopics);
+        var kafkaTopic = A.KafkaTopic.Build();
+        await repo.Add(kafkaTopic);
         await dbContext.SaveChangesAsync();
 
         var allObjects = await repo.GetAll();
 
         Assert.Single(allObjects);
-        Assert.Equal(id, allObjects[0].Id);
-        Assert.Equal("foo", allObjects[0].Foo);
-        Assert.Equal(1, allObjects[0].Bar);
+        Assert.Equal(kafkaTopic.Id, allObjects[0].Id);
+        Assert.Equal(kafkaTopic.Name, allObjects[0].Name);
+        Assert.Equal(kafkaTopic.Status, allObjects[0].Status);
+        Assert.Equal(kafkaTopic.CreatedAt, allObjects[0].CreatedAt);
+        Assert.Equal(kafkaTopic.CreatedBy, allObjects[0].CreatedBy);
+        Assert.Equal(kafkaTopic.KafkaClusterId, allObjects[0].KafkaClusterId);
     }
 }
