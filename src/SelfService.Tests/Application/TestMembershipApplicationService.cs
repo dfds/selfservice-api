6a2d9be6614da27ae084637182eafe43c9cb9337
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-10-26 16:39:36 +0200

    fixed test

--- src/SelfService.Tests/Application/TestMembershipApplicationService.cs
+++ src/SelfService.Tests/Application/TestMembershipApplicationService.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
-using SelfService.Application;
+using Moq;
 using SelfService.Domain.Exceptions;
 using SelfService.Domain.Models;
+using SelfService.Domain.Services;
 
 namespace SelfService.Tests.Application;
 
@@ -67,22 +68,47 @@ public class TestMembershipApplicationService
     {
         await using var databaseFactory = new InMemoryDatabaseFactory();
         var dbContext = await databaseFactory.CreateSelfServiceDbContext();
-        UserId userId = "chungus@dfds.com";
-        CapabilityId capabilityId = "reflect2improve-GPU-cluster-mgmt-qxyz";
         var membershipRepo = A.MembershipRepository.WithDbContext(dbContext).Build();
+        var membershipApplicationRepo = A.MembershipApplicationRepository.WithDbContext(dbContext).Build();
+        var capabilityRepo = A.CapabilityRepository.WithDbContext(dbContext).Build();
+
         var membershipApplicationService = A.MembershipApplicationService
             .WithMembershipRepository(membershipRepo)
+            .WithMembershipApplicationRepository(membershipApplicationRepo)
+            .WithCapabilityRepository(capabilityRepo)
             .Build();
 
-        await membershipApplicationService.JoinCapability(capabilityId, userId);
+        var capability = A.Capability.Build();
+        await capabilityRepo.Add(capability);
         await dbContext.SaveChangesAsync();
 
-        await Assert.ThrowsAsync<AlreadyHasActiveMembershipException>(
-            async () => await membershipApplicationService.JoinCapability(capabilityId, userId)
-        );
+        var approver = A.UserId;
+        await membershipRepo.Add(new Membership(MembershipId.New(), capability.Id, approver, DateTime.UtcNow));
         await dbContext.SaveChangesAsync();
 
-        var memberships = await dbContext.Memberships.ToListAsync();
-        Assert.Single(memberships);
+        var toJoin = UserId.Parse("bar");
+
+        await membershipApplicationService.SubmitMembershipApplication(capability.Id, toJoin);
+        await dbContext.SaveChangesAsync();
+
+        var application = await membershipApplicationRepo.FindPendingBy(capability.Id, toJoin);
+        Assert.NotNull(application);
+        var applicationId = application.Id;
+
+        var authService = new Mock<IAuthorizationService>();
+        authService.Setup(x => x.CanApprove(approver, application)).ReturnsAsync(true);
+
+        // We need to also check if we can approve the application
+        var membershipApplicationServiceWithAuth = A.MembershipApplicationService
+            .WithMembershipApplicationRepository(membershipApplicationRepo)
+            .WithAuthorizationService(authService.Object)
+            .Build();
+
+        await membershipApplicationServiceWithAuth.ApproveMembershipApplication(applicationId, approver);
+        await dbContext.SaveChangesAsync();
+
+        var applicationAfterApproval = await membershipApplicationRepo.FindPendingBy(capability.Id, toJoin);
+        Assert.NotNull(applicationAfterApproval);
+        Assert.NotEmpty(applicationAfterApproval.Approvals);
     }
 }
