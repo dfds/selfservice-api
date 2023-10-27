fcb3e451b19c627634ca8bbaf8a6e19628cb18b2
Andreas Frisch <andfris@dfds.com>
2023-10-27 09:58:53 +0200

    Hotfix: Remember to actually store the right names

--- db/migrations/20231004093000_remove_ecr_repositoryname.sql
+++ db/migrations/20231004093000_remove_ecr_repositoryname.sql
@@ -1,3 +1,4 @@
 -- 2023-10-04 09:30:00 : remove ecr repositoryname
+UPDATE "ECRRepository" SET "Name" = "RepositoryName";
 ALTER TABLE "ECRRepository" DROP COLUMN "RepositoryName";
 ALTER TABLE "ECRRepository" ADD UNIQUE ("Name");
