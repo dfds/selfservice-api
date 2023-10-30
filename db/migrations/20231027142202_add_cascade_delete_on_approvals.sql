20f33aee997a39395b302e6c5d3014d1e5bd0aba
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-10-30 10:13:12 +0100

    removed not necessary migration

--- db/migrations/20231027142202_add_cascade_delete_on_approvals.sql
+++
@@ -1,4 +0,0 @@
--- 2023-10-27 14:22:02 : add cascade delete on approvals
-
-ALTER TABLE "MembershipApproval"
-    ADD CONSTRAINT "MembershipApproval_MembershipApplicationId_FK" FOREIGN KEY ("MembershipApplicationId") REFERENCES "MembershipApplication" ("Id") ON DELETE CASCADE;
