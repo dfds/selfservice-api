340a2a5a9c9a8cda1ae2acb4330a41eb1bb064b5
Andreas Frisch <andfris@dfds.com>
2023-11-20 09:57:39 +0100

    Lets also compare correctly

--- src/SelfService/Application/PlatformDataApiRequesterService.cs
+++ src/SelfService/Application/PlatformDataApiRequesterService.cs
@@ -186,7 +186,7 @@ public class PlatformDataApiRequesterService : IPlatformDataApiRequesterService
         {
             if (mappedCosts.TryGetValue(myCapability.Id, out var myCosts))
             {
-                myCosts.ToList().Sort((x, y) => x.TimeStamp.CompareTo(x.TimeStamp));
+                myCosts.ToList().Sort((x, y) => x.TimeStamp.CompareTo(y.TimeStamp));
                 costs.Add(new CapabilityCosts(myCapability.Id, myCosts.ToArray()));
             }
         }
