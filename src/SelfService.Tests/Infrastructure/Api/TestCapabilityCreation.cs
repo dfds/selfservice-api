dab1235c9c6e570f61a62e5fdc92d49806090ee7
paulseghers <0099seghers@gmail.com>
2023-12-05 11:17:16 +0100

    changed commenting, maybe that's what csharpier is agry about?

--- src/SelfService.Tests/Infrastructure/Api/TestCapabilityCreation.cs
+++ src/SelfService.Tests/Infrastructure/Api/TestCapabilityCreation.cs
@@ -14,61 +14,62 @@ namespace SelfService.Tests.Infrastructure.Api;
 
 
 
-/* COMMENTING OUT BECAUSE THIS TEST IS FAILING AND WE DON'T KNOW WHY
-public class TestCapabilityCreation
-{
-    [Fact]
-    [Trait("Category", "Integration")]
-    public async Task creating_capability_also_populates_invites()
-    {
-        //setup
-        var databaseFactory = new ExternalDatabaseFactory();
-        var dbContext = await databaseFactory.CreateDbContext();
 
-        var capabilityRepo = A.CapabilityRepository.WithDbContext(dbContext).Build();
-        var invitationRepo = A.InvitationRepository.WithDbContext(dbContext).Build();
-
-        var invitationService = A.InvitationApplicationService
-            .WithDbContextAndDefaultRepositories(dbContext)
-            .WithInvitationRepository(invitationRepo)
-            .Build();
-
-        await using var application = new ApiApplication();
-        application.ReplaceService<IAwsAccountRepository>(new StubAwsAccountRepository());
-        application.ReplaceService<ICapabilityRepository>(new StubCapabilityRepository());
-        application.ReplaceService<IMembershipQuery>(new StubMembershipQuery());
-        application.ReplaceService<ICapabilityDeletionStatusQuery>(new StubCapabilityDeletionStatusQuery());
-        //await using var application = new ApiApplicationBuilder()
-        //   .WithAwsAccountRepository(new StubAwsAccountRepository())
-        //   .WithMembershipQuery(new StubMembershipQuery())
-        //   .Build();
-
-        string invitedUser = "foobar@mailinator.com";
-        UserId invitedUserId = UserId.Parse(invitedUser);
-
-        var invitations = await invitationService.GetActiveInvitationsForType(
-            userId: invitedUserId,
-            targetType: InvitationTargetTypeOptions.Capability
-        );
-        Assert.Empty(invitations);
-
-        string json = JsonSerializer.Serialize(
-            A.CapabilityRequest.WithName("cap-pop-inv-test").WithInvitees(new List<string> { invitedUser }).Build()
-        );
-        HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-        using var client = application.CreateClient();
-        var response = await client.PostAsync($"/capabilities", content);
-
-        // assertions
-        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-
-        var capabilitiesAfter = await capabilityRepo.GetAll();
-        Assert.True(capabilitiesAfter.ToList().Exists(x => x.Name == "cap-pop-inv-test"));
-
-        invitations = await invitationService.GetActiveInvitationsForType(
-            userId: invitedUserId,
-            targetType: InvitationTargetTypeOptions.Capability
-        );
-        Assert.Single(invitations);
-    }
-}*/
+// COMMENTING OUT BECAUSE THIS TEST IS FAILING AND WE DON'T KNOW WHY
+// public class TestCapabilityCreation
+// {
+//     [Fact]
+//     [Trait("Category", "Integration")]
+//     public async Task creating_capability_also_populates_invites()
+//     {
+//         //setup
+//         var databaseFactory = new ExternalDatabaseFactory();
+//         var dbContext = await databaseFactory.CreateDbContext();
+//
+//         var capabilityRepo = A.CapabilityRepository.WithDbContext(dbContext).Build();
+//         var invitationRepo = A.InvitationRepository.WithDbContext(dbContext).Build();
+//
+//         var invitationService = A.InvitationApplicationService
+//             .WithDbContextAndDefaultRepositories(dbContext)
+//             .WithInvitationRepository(invitationRepo)
+//             .Build();
+//
+//         await using var application = new ApiApplication();
+//         application.ReplaceService<IAwsAccountRepository>(new StubAwsAccountRepository());
+//         application.ReplaceService<ICapabilityRepository>(new StubCapabilityRepository());
+//         application.ReplaceService<IMembershipQuery>(new StubMembershipQuery());
+//         application.ReplaceService<ICapabilityDeletionStatusQuery>(new StubCapabilityDeletionStatusQuery());
+//         //await using var application = new ApiApplicationBuilder()
+//         //   .WithAwsAccountRepository(new StubAwsAccountRepository())
+//         //   .WithMembershipQuery(new StubMembershipQuery())
+//         //   .Build();
+//
+//         string invitedUser = "foobar@mailinator.com";
+//         UserId invitedUserId = UserId.Parse(invitedUser);
+//
+//         var invitations = await invitationService.GetActiveInvitationsForType(
+//             userId: invitedUserId,
+//             targetType: InvitationTargetTypeOptions.Capability
+//         );
+//         Assert.Empty(invitations);
+//
+//         string json = JsonSerializer.Serialize(
+//             A.CapabilityRequest.WithName("cap-pop-inv-test").WithInvitees(new List<string> { invitedUser }).Build()
+//         );
+//         HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+//         using var client = application.CreateClient();
+//         var response = await client.PostAsync($"/capabilities", content);
+//
+//         // assertions
+//         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+//
+//         var capabilitiesAfter = await capabilityRepo.GetAll();
+//         Assert.True(capabilitiesAfter.ToList().Exists(x => x.Name == "cap-pop-inv-test"));
+//
+//         invitations = await invitationService.GetActiveInvitationsForType(
+//             userId: invitedUserId,
+//             targetType: InvitationTargetTypeOptions.Capability
+//         );
+//         Assert.Single(invitations);
+//     }
+// }
