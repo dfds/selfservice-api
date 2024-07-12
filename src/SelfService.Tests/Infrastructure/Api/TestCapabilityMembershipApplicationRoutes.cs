c9c8d015756fd8f1647b30380d6f4703a612fde6
HrisyToteva <hristiqnatoteva9718@gmail.com>
2024-07-12 11:22:58 +0200

    fixed test

--- src/SelfService.Tests/Infrastructure/Api/TestCapabilityMembershipApplicationRoutes.cs
+++ src/SelfService.Tests/Infrastructure/Api/TestCapabilityMembershipApplicationRoutes.cs
@@ -310,7 +310,7 @@ public class TestCapabilityMembershipApplicationRoutes
         var applicationJson = document!.SelectElements("/items").SingleOrDefault();
         var allowedVerbs = applicationJson.SelectElements("/approvals/_links/self/allow").Select(x => x.GetString());
 
-        Assert.Equal(new[] { "GET", "POST" }, allowedVerbs);
+        Assert.Equal(new[] { "GET", "POST", "DELETE"}, allowedVerbs);
     }
 
     //get membershipapplications for a capability returns correct allow (with variations on approvals)
