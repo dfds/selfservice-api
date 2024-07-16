7fea2351da38412771c190616a1434317f5a8cf3
Andreas Frisch <andfris@dfds.com>
2024-07-16 14:30:22 +0200

    Take control of Aspectly functionality

---
+++ src/SelfService/Domain/Aspectly/PipelineSteps.cs
@@ -0,0 +1,13 @@
+namespace SelfService.Domain.Aspectly;
+
+internal class PipelineStep
+{
+    public PipelineStep(IAspect aspect, Attribute triggerAttribute)
+    {
+        Aspect = aspect;
+        TriggerAttribute = triggerAttribute;
+    }
+
+    public IAspect Aspect { get; private set; }
+    public Attribute TriggerAttribute { get; private set; }
+}
---
+++ src/SelfService/Domain/Aspectly/README.md
@@ -0,0 +1,4 @@
+Aspectly is a C# Framework for writing Aspect Oriented Programming.
+It is originally created by Jens Andersen and available as a NuGet package with documentation here: https://github.com/jensandresen/aspectly
+
+Due to lack of maintenance leading to security issues, we have transferred what was necessary for our code to work.
