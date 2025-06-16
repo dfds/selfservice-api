784bd2ca65958fae8dd9314d3c964dab3af7e89f
Emil H. Clausen <sequoiia@hummel.yt>
2025-06-16 11:20:36 +0200

    test: Make rbac test a bit more flexible

--- src/SelfService/Configuration/Rbac.cs
+++ src/SelfService/Configuration/Rbac.cs
@@ -8,7 +8,7 @@ public static class Rbac
     public static void AddRbac(this WebApplicationBuilder builder)
     { 
         // For testing purposes, TODO: replace with implementation that uses DB & repository
-        builder.Services.AddTransient<IRbacApplicationService, RbacApplicationService>(_ => RbacApplicationService.BootstrapTestService());
+        builder.Services.AddTransient<IRbacApplicationService, RbacApplicationService>();
         builder.Services.AddTransient<AuthChecker>();
     }
 }
