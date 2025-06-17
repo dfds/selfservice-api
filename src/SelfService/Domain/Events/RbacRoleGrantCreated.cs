ad376c401c0c3935b3831ec04e49d6f6b41191b8
Emil H. Clausen <sequoiia@hummel.yt>
2025-06-17 11:46:46 +0200

    wip: Added DB types for RbacRoleGrant and RbacPermissionGrant

---
+++ src/SelfService/Domain/Events/RbacRoleGrantCreated.cs
@@ -0,0 +1,9 @@
+using SelfService.Domain.Models;
+
+namespace SelfService.Domain.Events;
+
+public class RbacRoleGrantCreated : IDomainEvent
+{
+    public const string EventType = "rbac-role-grant-created";
+    
+}
