d76b5ea47c177e175fbab67fba571886afdcf3d8
Emil H. Clausen <sequoiia@hummel.yt>
2025-06-19 21:14:19 +0200

    wip: rbac

--- src/SelfService/Application/IRbacApplicationService.cs
+++ src/SelfService/Application/IRbacApplicationService.cs
@@ -1,7 +1,11 @@
+using SelfService.Domain.Models;
+
 namespace SelfService.Application;
 
 public interface IRbacApplicationService
 {
     Task<PermittedResponse> IsUserPermitted(string user, List<Permission> permissions, string objectId);
     List<AccessPolicy> GetApplicablePoliciesUser(string user);
+    Task<List<RbacPermissionGrant>> GetPermissionGrantsForUser(string user);
+    Task<List<RbacRoleGrant>> GetRoleGrantsForUser(string user);
 }
