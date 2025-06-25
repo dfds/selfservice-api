976de34a68ec4c67e40ae96e08ba0bd4df6f062d
Andreas Frisch <andfris@dfds.com>
2025-06-25 13:10:21 +0200

    Simple route test for release notes

---
+++ src/SelfService.Tests/Infrastructure/Api/TestReleaseNoteRoutes.cs
@@ -0,0 +1,58 @@
+
+using System.Text.Json;
+using SelfService.Domain.Models;
+using SelfService.Domain.Queries;
+using SelfService.Tests.TestDoubles;
+
+namespace SelfService.Tests.Infrastructure.Api;
+
+public class TestReleaseNoteRoutes
+{
+    [Fact]
+    public async Task get_release_note_by_id_returns_expected_permissions_on_toggle_active_link()
+    {
+        var stubReleaseNote = A.ReleaseNote.Build();
+
+        await using var application = new ApiApplicationBuilder()
+            .WithReleaseNoteRepository(new StubReleaseNoteRepository(stubReleaseNote))
+            .Build();
+
+        using var client = application.CreateClient();
+        var response = await client.GetAsync($"/release-notes/{stubReleaseNote.Id}");
+
+        var content = await response.Content.ReadAsStringAsync();
+        var document = JsonSerializer.Deserialize<JsonDocument>(content);
+
+        var allowValues = document
+            ?.SelectElement("/_links/toggleIsActive/allow")
+            ?.EnumerateArray()
+            .Select(x => x.GetString() ?? "")
+            .ToArray();
+
+        Assert.Empty(allowValues!);
+    }
+
+    [Fact]
+    public async Task get_release_note_list_returns_expected_permissions_on_create_link()
+    {
+        await using var application = new ApiApplicationBuilder().Build();
+
+        using var client = application.CreateClient();
+        var response = await client.GetAsync($"/release-notes");
+
+        var content = await response.Content.ReadAsStringAsync();
+        var document = JsonSerializer.Deserialize<JsonDocument>(content);
+
+        var allowValues = document
+            ?.SelectElement("/_links/createReleaseNote/allow")
+            ?.EnumerateArray()
+            .Select(x => x.GetString() ?? "")
+            .ToArray();
+
+        Assert.Empty(allowValues!);
+    }
+
+    // [andfris: 2025-06-25] TODO: Add tests for cloud-engineer permissions.
+    // This is not currently supported by the test setup, so it requires additional work to implement.
+    // For now it is enough to know, that permissions are empty by default.
+}
