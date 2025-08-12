82564cf027ee3dc65d804318697b440d54b471bb
Emil H. Clausen <sequoiia@hummel.yt>
2025-08-12 13:57:36 +0200

    wip: Continue work on RBAC API controller

---
+++ src/SelfService/Infrastructure/Api/RBAC/RbacGroupApiResource.cs
@@ -0,0 +1,28 @@
+using System.Text.Json.Serialization;
+using SelfService.Domain.Models;
+using SelfService.Infrastructure.Api.Capabilities;
+
+namespace SelfService.Infrastructure.Api.RBAC;
+
+public class RbacGroupApiResource
+{
+    public string Id { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public ICollection<RbacGroupMember> Members  { get; set; } = new List<RbacGroupMember>();
+    
+    [JsonPropertyName("_links")]
+    public required RLinks Links { get; set; }
+    
+    public class RLinks
+    {
+        public ResourceLink Self { get; set; }
+
+        public RLinks(ResourceLink self)
+        {
+            Self = self;
+        }
+    }
+}
---
+++ src/SelfService/Infrastructure/Api/RBAC/RbacRoleGrantApiResource.cs
@@ -0,0 +1,6 @@
+namespace SelfService.Infrastructure.Api.RBAC;
+
+public class RbacRoleGrantApiResource
+{
+    
+}
