81520f441359a06113b9d4012b829be031f7ade4
Andreas Frisch <andfris@dfds.com>
2025-11-04 17:31:06 +0100

    Test leaving of capabilities better

--- src/SelfService.Tests/Infrastructure/Api/TestCapabilityMembershipsRoutes.cs
+++ src/SelfService.Tests/Infrastructure/Api/TestCapabilityMembershipsRoutes.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using SelfService.Application;
 using SelfService.Domain.Models;
 using SelfService.Domain.Queries;
+using SelfService.Infrastructure.Persistence.Queries;
 using SelfService.Tests.TestDoubles;
 
 namespace SelfService.Tests.Infrastructure.Api;
@@ -9,18 +10,112 @@ namespace SelfService.Tests.Infrastructure.Api;
 public class TestCapabilityMembershipsRoutes
 {
     [Fact]
-    public async Task get_capability_by_id_returns_expected_leave_with_multiple_members()
+    public async Task get_capability_by_id_returns_expected_non_owner_leave_always()
     {
         var stubCapability = A.Capability.Build();
+        
+        await using var application = new ApiApplicationBuilder()
+            .WithAwsAccountRepository(new StubAwsAccountRepository())
+            .WithCapabilityRepository(new StubCapabilityRepository(stubCapability))
+            .ConfigureRbac()
+            .Build();
+
+        application.ReplaceService<IRbacPermissionGrantRepository>(new StubRbacPermissionGrantRepository());
+        application.ReplaceService<IRbacRoleGrantRepository>(new StubRbacRoleGrantRepository());
+        application.ReplaceService<IPermissionQuery>(new StubPermissionQuery());
+        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(isPermitted: false));
+
+        using var client = application.CreateClient();
+        var response = await client.GetAsync($"/capabilities/{stubCapability.Id}");
+
+        var content = await response.Content.ReadAsStringAsync();
+        var document = JsonSerializer.Deserialize<JsonDocument>(content);
+
+        var allowValues = document
+            ?.SelectElement("/_links/leaveCapability/allow")
+            ?.EnumerateArray()
+            .Select(x => x.GetString() ?? "")
+            .ToArray();
+
+        Assert.Equal(["GET", "POST"], allowValues);
+    }
+
+    [Fact]
+    public async Task get_capability_by_id_forbids_owner_leave_with_single_owner()
+    {
+        var stubCapability = A.Capability.Build();
+        var ownerRole = A.RbacRole.WithName("Owner").WithAccessType(RbacAccessType.Capability).Build();
 
         await using var application = new ApiApplicationBuilder()
             .WithAwsAccountRepository(new StubAwsAccountRepository())
             .WithCapabilityRepository(new StubCapabilityRepository(stubCapability))
-            .WithMembershipQuery(new StubMembershipQuery(hasActiveMembership: true, hasMultipleMembers: true))
+            .WithMembershipQuery(new StubMembershipQuery(hasActiveMembership: true))
             .Build();
         application.ReplaceService<IRbacPermissionGrantRepository>(new StubRbacPermissionGrantRepository());
         application.ReplaceService<IRbacRoleGrantRepository>(new StubRbacRoleGrantRepository());
-        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(isPermitted: true));
+        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(
+            assignableRoles: [
+                ownerRole
+            ],
+            roleGrants:
+            [
+                A.RbacRoleGrant
+                    .WithRoleId(ownerRole.Id)
+                    .AssignToUser("foo@bar.com")
+                    .AssignedForCapability(stubCapability.Id)
+                    .Build()
+            ],
+            isPermitted: false // doesn't matter in this case
+        ));
+        application.ReplaceService<IPermissionQuery>(new StubPermissionQuery());
+
+        using var client = application.CreateClient();
+        var response = await client.GetAsync($"/capabilities/{stubCapability.Id}");
+
+        var content = await response.Content.ReadAsStringAsync();
+        var document = JsonSerializer.Deserialize<JsonDocument>(content);
+
+        var allowValues = document
+            ?.SelectElement("/_links/leaveCapability/allow")
+            ?.EnumerateArray()
+            .Select(x => x.GetString() ?? "")
+            .ToArray();
+
+        Assert.Equal(new[] { "GET" }, allowValues);
+    }
+
+[Fact]
+    public async Task get_capability_by_id_allows_owner_leave_with_multiple_owners()
+    {
+        var stubCapability = A.Capability.Build();
+        var ownerRole = A.RbacRole.WithName("Owner").WithAccessType(RbacAccessType.Capability).Build();
+
+        await using var application = new ApiApplicationBuilder()
+            .WithAwsAccountRepository(new StubAwsAccountRepository())
+            .WithCapabilityRepository(new StubCapabilityRepository(stubCapability))
+            .WithMembershipQuery(new StubMembershipQuery(hasActiveMembership: true))
+            .Build();
+        application.ReplaceService<IRbacPermissionGrantRepository>(new StubRbacPermissionGrantRepository());
+        application.ReplaceService<IRbacRoleGrantRepository>(new StubRbacRoleGrantRepository());
+        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(
+            assignableRoles: [
+                ownerRole
+            ],
+            roleGrants:
+            [
+                A.RbacRoleGrant
+                    .WithRoleId(ownerRole.Id)
+                    .AssignToUser("foo@bar.com")
+                    .AssignedForCapability(stubCapability.Id)
+                    .Build(),
+                A.RbacRoleGrant
+                    .WithRoleId(ownerRole.Id)
+                    .AssignToUser("bar@foo.com")
+                    .AssignedForCapability(stubCapability.Id)
+                    .Build()
+            ],
+            isPermitted: false // doesn't matter in this case
+        ));
         application.ReplaceService<IPermissionQuery>(new StubPermissionQuery());
 
         using var client = application.CreateClient();
@@ -39,36 +134,6 @@ public class TestCapabilityMembershipsRoutes
     }
 
     [Fact]
-    public async Task get_capability_by_id_returns_expected_leave_with_single_member()
-    {
-        var stubCapability = A.Capability.Build();
-
-        await using var application = new ApiApplicationBuilder()
-            .WithAwsAccountRepository(new StubAwsAccountRepository())
-            .WithCapabilityRepository(new StubCapabilityRepository(stubCapability))
-            .WithMembershipQuery(new StubMembershipQuery(hasActiveMembership: true))
-            .Build();
-        application.ReplaceService<IRbacPermissionGrantRepository>(new StubRbacPermissionGrantRepository());
-        application.ReplaceService<IRbacRoleGrantRepository>(new StubRbacRoleGrantRepository());
-        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(isPermitted: true));
-        application.ReplaceService<IPermissionQuery>(new StubPermissionQuery());
-
-        using var client = application.CreateClient();
-        var response = await client.GetAsync($"/capabilities/{stubCapability.Id}");
-
-        var content = await response.Content.ReadAsStringAsync();
-        var document = JsonSerializer.Deserialize<JsonDocument>(content);
-
-        var allowValues = document
-            ?.SelectElement("/_links/leaveCapability/allow")
-            ?.EnumerateArray()
-            .Select(x => x.GetString() ?? "")
-            .ToArray();
-
-        Assert.Equal(new[] { "GET" }, allowValues);
-    }
-
-    [Fact]
     public async Task resource_links_dont_contain_post_if_capability_is_pending_deletion()
     {
         var stubCapability = A.Capability.Build();
@@ -80,7 +145,7 @@ public class TestCapabilityMembershipsRoutes
             .Build();
         application.ReplaceService<IRbacPermissionGrantRepository>(new StubRbacPermissionGrantRepository());
         application.ReplaceService<IRbacRoleGrantRepository>(new StubRbacRoleGrantRepository());
-        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(isPermitted: true));
+        application.ReplaceService<IRbacApplicationService>(new StubRbacApplicationService(isPermitted: false));
         application.ReplaceService<IPermissionQuery>(new StubPermissionQuery());
 
         using var client = application.CreateClient();
