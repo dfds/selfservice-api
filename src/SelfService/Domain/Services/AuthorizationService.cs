65692edfcca72194d0b25d60f383363549fa3913
Andreas Frisch <andreas.frisch@gmail.com>
2026-06-03 15:27:24 +0200

    initial

--- src/SelfService/Domain/Services/AuthorizationService.cs
+++ src/SelfService/Domain/Services/AuthorizationService.cs
@@ -646,6 +646,17 @@ public class AuthorizationService : IAuthorizationService
         return IsCloudEngineerEnabled(portalUser);
     }
 
+    private static readonly IReadOnlySet<string> BatchCapabilityAllowList = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    {
+        "andfris@dfds.com",
+    };
+
+    public bool CanBatchCreateCapabilities(PortalUser portalUser)
+    {
+        return IsCloudEngineerEnabled(portalUser)
+            || BatchCapabilityAllowList.Contains(portalUser.Id.ToString());
+    }
+
     public async Task<bool> CanDeleteMembershipApplication(
         PortalUser portalUser,
         UserId userId,
